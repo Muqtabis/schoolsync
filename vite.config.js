23ed2a32d4bd2f75b0b80eee28e9af3995f9deea
MADMAG0611 <ahmarmuqtabis@gmail.com>
2025-09-19 01:41:10 +0530

    Trigger fresh redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default {
+export default defineConfig({
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
-}
+});
 
 
